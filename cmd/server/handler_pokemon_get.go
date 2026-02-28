b34a6167e27f34f65448b3e2fb99c48a61ae779f
Eric Contino <evcontino5107@gmail.com>
2026-02-27 19:19:01 -0500

    fixed gitignore to include cmd/server

---
+++ cmd/server/handler_pokemon_get.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/EricContino/pokemon-guess-who/internal/components"
+)
+
+func (cfg *apiConfig) handlerPokemonGet(w http.ResponseWriter, r *http.Request) {
+	// type parameters struct {
+	// 	Gens []int32 `json:"gens"`
+	// }
+
+	// decoder := json.NewDecoder(r.Body)
+	// params := parameters{}
+	// err := decoder.Decode(&params)
+	// if err != nil {
+	// 	respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+	// 	return
+	// }
+	gens := []int32{1}
+	pokemon, err := cfg.db.GetPokemonByGen(r.Context(), gens)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't get chirp", err)
+		return
+	}
+
+	component := components.PokemonList(pokemon)
+	component.Render(r.Context(), w)
+}
